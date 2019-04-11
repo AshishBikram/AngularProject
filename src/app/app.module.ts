a5462cf89397a543ef5735de7a54b0f79e51d3a5
Ashish BIkram <bikram.ashish@gmail.com>
2019-04-11 22:59:53 +0545

    fdasdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    AddEmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      HttpClientModule,
+      AppRoutingModule,
+      ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
